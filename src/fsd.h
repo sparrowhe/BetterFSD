7a058e79e40ec211f51394a8bf5f189f06edf017
LinkTechTips <3174327625@qq.com>
2022-11-15 02:10:12 +0800

    Add the wheazzup in json format,fix typo in config

--- src/fsd.h
+++ src/fsd.h
@@ -14,6 +14,7 @@ class fsd
    pman *pmanager;
    char *certfile;
    char *whazzupfile;
+   char *whazzupjsonfile;
    time_t timer, prevnotify, prevlagcheck, certfilestat, prevcertcheck;
    void configmyserver();
    void configure();
